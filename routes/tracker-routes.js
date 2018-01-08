1a68c93eb347b89d32780cc8f0533975cd0270fa
Joshua Russell <jkrussell756@gmail.com>
2018-01-08 12:28:48 -0500

    routes

---
+++ routes/tracker-routes.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const Router = express.Router();
+const controller = require('../controllers/tracker-controller');
+
+Router.get('/', controller.login)
+Router.get('/:username', controller.tracker);
+Router.get('/:username/new', controller.new);
+Router.get('/:username/:currency_id', controller.show);
+Router.get('/:username/:currency_id/edit', controller.edit);
+Router.put('/:username/currency_id', controller.update);
+Router.post('/:username', controller.create);
+Router.delete('/:username/:currency_id', controller.destroy);
+
+module.exports = Router;
